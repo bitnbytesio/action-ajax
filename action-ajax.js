9e9581edd57ee14d9f6f7b8269f0a6c076b90d91
Harcharan Singh <artisangang@gmail.com>
2015-04-22 23:09:40 +0530

    init

--- action-ajax.js
+++ action-ajax.js
@@ -4,3 +4,35 @@
  * @version 2
  * @git: https://github.com/artisangang/action-ajax
  */
+
+;(function(){
+
+	var actionAjax = actionAjax || {};
+	
+	function actionAjax(config) {
+		
+		var defaults = {
+				url: "",
+				method: "get",
+				trigger: "click",
+				container: "#action-ajax-container",
+				append: false,
+				contentType: "application/x-www-form-urlencoded; charset=UTF-8", 
+				after: function() {},
+				before: function() {},
+				success: function() {},
+				fail: function() {},
+				
+			};
+		
+		this.config = $.extend({}, defauls, config);
+	
+	}
+	
+	actionAjax.prototype.call = function() {
+	
+		
+	
+	};
+	
+});
